273c5dccc8354e48aab08ecf69d6e373caed4f8e
Eleanor Brodie <eleanorbrodie@Eleanors-MacBook-Pro.local>
2016-07-16 16:43:24 +0100

    Add react client

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 import {resolve} from 'path';
+import {readFileSync} from 'fs';
 
 var express = require('express');
 
 var app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  const prepare = require('prepare-response');
+  const client = prepare(readFileSync(__dirname + '/index.js'), {'content-type': 'js'})
+  app.get('/client.js', (req, res, next) => {
+    client.send(req, res, next);
+  });
+} else {
+  var browserify = require('browserify-middleware');
+  app.get('/client.js', browserify(__dirname + '/index.js', {transform: require('babelify')}))
+}
+
+
 
 app.get('/', (req, res, next) => {
   res.sendFile(resolve(__dirname + '/../index.html'));
